a7deb41c37a54f5fe03aa761325493257deb2b78
harshitarawat673-sketch <harshitarawat673@gmail.com>
2026-07-17 23:49:12 +0530

    mongodb connection

---
+++ models/Url.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const urlSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    originalUrl: {
+        type: String,
+        required: true
+    },
+    shortUrl: {
+        type: String,
+        required: true
+    },
+    clicks: {
+        type: Number,
+        default: 0
+    },
+    lastAccessed: {
+        type: Date,
+        default: null
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("Url", urlSchema);
--- server.js
+++ server.js
@@ -1,25 +1,12 @@
-
+require("dotenv").config();
 const express = require("express");
-const fs = require("fs");
+const mongoose = require("mongoose");
 const { nanoid } = require("nanoid");
-
+const Url = require("./models/Url");
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-
-const DATA_FILE = "./data.json";
-
-// Read data from JSON
-function readData() {
-  const data = fs.readFileSync(DATA_FILE);
-  return JSON.parse(data);
-}
-
-// Write data to JSON
-function writeData(data) {
-  fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
-}
 
 // HOME
 app.get("/", (req, res) => {
@@ -27,103 +14,116 @@ app.get("/", (req, res) => {
 });
 
 // CREATE SHORT URL
-app.post("/shorten", (req, res) => {
-  const { originalUrl } = req.body;
+app.post("/shorten", async (req, res) => {
+  try {
+    console.log("Request Body:", req.body);
 
-  if (!originalUrl) {
-    return res.status(400).json({
-      message: "URL is required",
+    const { originalUrl } = req.body;
+
+    if (!originalUrl) {
+      return res.status(400).json({
+        message: "URL is required",
+      });
+    }
+     try {
+            new URL(originalUrl);
+        } catch {
+            return res.status(400).json({
+                message: "Invalid URL"
+            });
+        }
+    const shortCode = nanoid(6);
+
+    const newUrl = await Url.create({
+      id: shortCode,
+      originalUrl,
+      shortUrl: `http://localhost:${PORT}/${shortCode}`,
+    });
+
+    console.log("Saved:", newUrl);
+
+    res.status(201).json({
+      message: "Short URL created",
+      data: newUrl,
+    });
+  } catch (err) {
+    console.error(err);   
+    res.status(500).json({
+      message: err.message,
     });
   }
-
-  const data = readData();
-
-  const shortCode = nanoid(6);
-
-  const newUrl = {
-    id: shortCode,
-    originalUrl,
-    shortUrl: `http://localhost:${PORT}/${shortCode}`,
-    clicks: 0,
-    lastAccessed: null,
-    createdAt: new Date(),
-  };
-
-  data.urls.push(newUrl);
-
-  writeData(data);
-
-  res.status(201).json({
-    message: "Short URL created",
-    data: newUrl,
-  });
 });
 
 // GET ALL URL ANALYTICS
-app.get("/analytics/all", (req, res) => {
-  const data = readData();
+app.get("/analytics/all", async (req, res) => {
 
-  res.json(data.urls);
+    const urls = await Url.find();
+
+    res.json(urls);
+
 });
 
 // TOP 5 MOST CLICKED URLS
-app.get("/analytics/top", (req, res) => {
-  const data = readData();
+app.get("/analytics/top", async (req, res) => {
 
-  const topUrls = [...data.urls]
-    .sort((a, b) => b.clicks - a.clicks)
-    .slice(0, 5);
+    const topUrls = await Url.find()
+        .sort({ clicks: -1 })
+        .limit(5);
 
-  res.json(topUrls);
+    res.json(topUrls);
+
 });
 
 // SINGLE URL ANALYTICS
-app.get("/analytics/:code", (req, res) => {
-  const { code } = req.params;
+app.get("/analytics/:code", async (req, res) => {
 
-  const data = readData();
-
-  const url = data.urls.find((u) => u.id === code);
-
-  if (!url) {
-    return res.status(404).json({
-      message: "URL not found",
+    const url = await Url.findOne({
+        id: req.params.code
     });
-  }
 
-  res.json({
-    originalUrl: url.originalUrl,
-    shortUrl: url.shortUrl,
-    clicks: url.clicks,
-    lastAccessed: url.lastAccessed,
-    createdAt: url.createdAt,
-  });
+    if (!url) {
+        return res.status(404).json({
+            message: "URL not found"
+        });
+    }
+
+    res.json(url);
+
 });
 
 // REDIRECT TO ORIGINAL URL
-app.get("/:code", (req, res) => {
-  const { code } = req.params;
+app.get("/:code", async (req, res) => {
 
-  const data = readData();
-
-  const url = data.urls.find((u) => u.id === code);
-
-  if (!url) {
-    return res.status(404).json({
-      message: "URL not found",
+    const url = await Url.findOne({
+        id: req.params.code
     });
-  }
 
-  // Analytics
-  url.clicks += 1;
-  url.lastAccessed = new Date();
+    if (!url) {
+        return res.status(404).json({
+            message: "URL not found"
+        });
+    }
 
-  writeData(data);
+    url.clicks++;
 
-  res.redirect(url.originalUrl);
+    url.lastAccessed = new Date();
+
+    await url.save();
+
+    res.redirect(url.originalUrl);
+
 });
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log(" MongoDB Connected");
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err);
+  });
+  app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
 
